bef7197915d1344226a423d581ebe61fbc0b259d
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-11-06 17:01:19 +0600

    loader size fix and minor logout fix

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,8 @@ const userContainer = document.getElementById('userContainer');
 const userContainer1 = document.getElementById('userContainer1');
 
 const signoutBtn = document.getElementById('signout');
+const signoutBtn1 = document.getElementById('signout1');
+
 
 
 let isUserLoggedin =  localStorage.getItem('isUserLoggedin');
@@ -27,4 +29,10 @@ signoutBtn.addEventListener('click', (event) => {
     localStorage.setItem('isUserLoggedin', false);
     location.reload();
 });
+
+signoutBtn1.addEventListener('click', (event) => {
+    localStorage.setItem('isUserLoggedin', false);
+    location.reload();
+});
+
 
