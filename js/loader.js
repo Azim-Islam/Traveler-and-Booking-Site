31ba91dde5153186067d351e3ee027be823f3820
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-11-06 15:04:04 +0600

    loader done

--- js/loader.js
+++ js/loader.js
@@ -1,14 +1,16 @@
 
 document.onreadystatechange = function () {
     const loadingSection = document.getElementById('loading');
-    const body = document.body;
+
     if (document.readyState !== "complete") {
         console.log('loading has begun');
         loadingSection.classList.replace('d-none', 'd-flex');
         document.body.classList.add('overflow-hidden');
     } else {
-        console.log('loading has ended');
-        loadingSection.classList.replace('d-flex', 'd-none');
-        document.body.classList.remove('overflow-hidden');
+        setTimeout(() => {
+            loadingSection.classList.replace('d-flex', 'd-none');
+            // loadingSection.classList.add('invisible');
+            document.body.classList.remove('overflow-hidden');
+        }, 500);
     }
  };
